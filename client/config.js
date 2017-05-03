492bc8e53ef5c9327fa57c5491f1e648fd1c3544
Tony Spiro <tspiro@tonyspiro.com>
2017-05-02 21:11:04 -0500

    default bucket edit

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 export default {
   bucket: {
-    slug: process.env.COSMIC_BUCKET || 'react-redux-node-todo-app',
+    slug: process.env.COSMIC_BUCKET || 'todo-app',
     type_slug: 'tasks',
     read_key: process.env.COSMIC_READ_KEY,
     write_key: process.env.COSMIC_WRITE_KEY
