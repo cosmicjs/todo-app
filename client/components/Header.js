8ec968a92774bb42895d1d50891a4e7f717244e2
Tony Spiro <tspiro@tonyspiro.com>
2017-05-02 21:12:55 -0500

    autocomplete off

--- client/components/Header.js
+++ client/components/Header.js
@@ -15,7 +15,7 @@ const Header = (props) => {
         }>
           <div className="form-group">
             <label for="exampleInputEmail1">Add New To-Do</label>
-            <input className="form-control input-lg" name="taskName" placeholder="Enter new task" />
+            <input autoComplete="off" className="form-control input-lg" name="taskName" placeholder="Enter new task" />
           </div>
           <button type="submit">Add</button>
         </form>
