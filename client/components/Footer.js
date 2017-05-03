8a2d309ca718ccb6b8f1021459b16fa4feec219c
Tony Spiro <tspiro@tonyspiro.com>
2017-05-02 19:55:46 -0500

    footer copy edit

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = (props) => {
         <div className="col-xs-12 text-center">
           <a href="https://cosmicjs.com" style={{textDecoration: "none", borderBottom: "none"}}>
             <img src="https://cosmicjs.com/images/logo.svg"/>
-            <p>Proudly powered by CosmicJs</p>
+            <p>Proudly powered by Cosmic JS</p>
           </a>
         </div>
       </div>
